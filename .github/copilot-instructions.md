9fa1827d56d265e396c74536d82618ca95aa64ef
Cody gilbert <146819086+codgilly@users.noreply.github.com>
2025-03-10 07:14:04 +0000

    fixed time slow

--- .github/copilot-instructions.md
+++ .github/copilot-instructions.md
@@ -2,7 +2,7 @@
 - Project name: ChronoCrusader
 - Unity version: Unity 6000.0.23f1
 - Active game object:
-  - Name: EnemyMeleeTest (1)
-  - Tag: Enemy
+  - Name: Player
+  - Tag: nothing
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/Player/Skills/TimeSwap.cs
+++ Assets/Scripts/Player/Skills/TimeSwap.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class TimeSwap : MonoBehaviour
 {
-    public bool isInPast;
+    public bool isInFuture;
 
-    public GameObject player;
+    //public Transform player;
 
     public Transform pastPlane;
     public Transform futurePlane;
@@ -12,19 +12,18 @@ public class TimeSwap : MonoBehaviour
     CharacterController cc;
     void Start()
     {
-       isInPast = true;
+        isInFuture = true;
        cc= GetComponent<CharacterController>();
     }
 
     void Update()
     {
-
         if(Input.GetKeyDown(KeyCode.Tab) )
         {
             //toggle the bool 
-            isInPast = isInPast?false:true;
+            isInFuture = isInFuture ?false:true;
 
-            if( isInPast)
+            if( isInFuture)
             {
                 GoToPast();
             }
@@ -38,16 +37,27 @@ public class TimeSwap : MonoBehaviour
 
     void GoToPast()
     {
+        Vector3 myPositionY = transform.position;
+        Vector3 futurePlaneDistanceY = futurePlane.transform.position;
+        //float yDistance = Mathf.Abs(myPosition.y - otherPosition.y);
+
+        float distance = Mathf.Abs(myPositionY.y - futurePlaneDistanceY.y);
+
         print("Method started - up");
-        transform.position = new Vector3(transform.position.x, pastPlane.transform.position.y + 1.725f, transform.position.z);
+        transform.position = new Vector3(transform.position.x, pastPlane.transform.position.y + distance, transform.position.z);
         Physics.SyncTransforms();
 
     }
 
     void GoToFuture()
     {
+        Vector3 myPosition = transform.position;
+        Vector3 pastPlaneDistance = pastPlane.transform.position;
+
+        float distance = Mathf.Abs(myPosition.y - pastPlaneDistance.y);
+
         print("Method started - down");
-        transform.position = new Vector3(transform.position.x, futurePlane.transform.position.y + 1.725f, transform.position.z);
+        transform.position = new Vector3(transform.position.x, futurePlane.transform.position.y + distance, transform.position.z);
         Physics.SyncTransforms();
     }
 
