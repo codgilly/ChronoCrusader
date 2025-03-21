a5b340d9c24af7900d1c193fb115c8968285a57e
Cody gilbert <146819086+codgilly@users.noreply.github.com>
2025-03-21 11:32:36 +0000

    started first level

--- Assets/Scripts/UI/LoadingSC.cs
+++ Assets/Scripts/UI/LoadingSC.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadingSC : MonoBehaviour
 {
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
