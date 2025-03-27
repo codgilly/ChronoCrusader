3eeedbafe517ebe2bc2b224db6dc2882ae144cd9
Cody gilbert <146819086+codgilly@users.noreply.github.com>
2025-03-27 09:29:21 +0000

    123

--- Assets/Scripts/UI/Options.cs
+++ Assets/Scripts/UI/Options.cs
@@ -2,26 +2,7 @@ using UnityEngine;
 
 public class Options : MonoBehaviour
 {
-    public static Options instance;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        if (instance == null)
-        {
-            // if instance is null, store a reference to this instance
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-            print("dont destroy");
-        }
-        else
-        {
-            // Another instance of this gameobject has been made so destroy it
-            // as we already have one
-            print("do destroy");
-            Destroy(gameObject);
-        }
-    }
-
+   
     // Update is called once per frame
     void Update()
     {
