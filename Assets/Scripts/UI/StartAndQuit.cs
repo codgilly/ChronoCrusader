1a0fd893d3140414a1c63ab1585c32038bfef0fc
Cody gilbert <146819086+codgilly@users.noreply.github.com>
2025-04-27 21:58:52 +0100

    213
    qw

--- Assets/Scripts/UI/StartAndQuit.cs
+++ Assets/Scripts/UI/StartAndQuit.cs
@@ -5,10 +5,37 @@ using UnityEngine.SceneManagement;
 public class StartAndQuit : MonoBehaviour
 {
     private int nextSceneToLoad;
+
+    private int thisScene;
+
+    public GameObject pauseScreen;
     void Start()
     {
+        thisScene = SceneManager.GetActiveScene().buildIndex;
         nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && thisScene != 0)
+        {
+            pauseScreen.SetActive(true);
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Time.timeScale = 0;
+        }
+
+    }
+
+    public void Resume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1;
+    }
+
+
     public void Quit()
     {
         Application.Quit();
@@ -17,5 +44,10 @@ public class StartAndQuit : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene(nextSceneToLoad);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
